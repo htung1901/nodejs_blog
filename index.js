b1191263f4754b7411c48f5dd61e7a9552174510
htung1901 <nvht2004@gmail.com>
2025-05-03 06:51:06 +0700

    Add morgan middleware for logging and update package dependencies

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const port = 3000
 
-app.get('/tin-tuc', (req, res) => {
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port http://localhost:${port}`)
 })
